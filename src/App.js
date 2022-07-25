65e6877247c62c0e5a176af8d41668e729c78e8d
patricioprietogaray <patricioprietogaray@gmail.com>
2022-07-25 10:51:17 -0300

    clase1

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ function App() {
   // todo este jsx que se ve a continuacion se insertará
   // en el id root en public/index.html
   
-  // los estilos deben estar en un objeto
+  // los estilos deben estar en un objeto coma no punto y coma como css ES OBJETO 
   const estiloCSS={
     color: "blue",
     margin: "1rem",
     padding: 5,
   }
   const sinVinieta = {
+    // es JSX no CSS por eso las propiedades es camelCase y no por guiones font-size -> fontSize
+    // tambien cambia los valores: font-size: 10px -> fontSize: 10 (sin px)
     listStyleType: "none",
   }
 
@@ -40,6 +42,11 @@ function App() {
       {/* se pueden injectar css directo (inline) en vez de llamar al objeto con doble llave {{}} 
        atenti que tiene prioridad inline que desde el archivo css, como con html y css, PUEDO AGREGAR
        LOGICA*/}
+
+       {/* inline tiene mayor prioridad que el css en app.css en react es comun hacerlo inline
+       porque se puede agregar logica (huboError) , tambien se puede hacer con clases
+       <div className = {esTemaOscuro ? 'oscuro' : 'claro' }>
+       donde oscuro y claro son dos clases distintas*/}
         <h2 style={{
             color: "white",
             backgroundColor: huboError ? "red" : "blue",
@@ -49,8 +56,13 @@ function App() {
 
 
       {/* </header> */}
+      {/* paso el estilo que se encuentra en un objeto de manera inline entre {} */}
       <ul style={sinVinieta}>
+        {/* lo que se ve a continuacion es JSX una mezcla de html y js para react */}
         {
+          // key es un elemento único que debe estar presente porque el virtual dom actualiza
+          // solo lo minimo indispensable, es por eso que el trabajo se facilitara en la renderizacion
+          // si se incluye key
           autos.map(auto => (<li key={auto.id}>
             <strong>{auto.marca}</strong>
             <span>{auto.nombre}</span>
